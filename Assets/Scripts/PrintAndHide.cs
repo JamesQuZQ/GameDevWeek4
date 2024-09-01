1050c7086936b22201ef9911390c1c9e0c6e2bdb
JamesQuZQ <jamesquzr@outlook.com>
2024-09-01 19:14:44 +1000

    Finished 100 band

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
-    private int counter = 3;
+    private int i = 3;
 
     public Renderer rend;
+
+    private int blueTerminator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        blueTerminator = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + ":" + ++counter);
+        Debug.Log(gameObject.name + ":" + ++i);
+        if (gameObject.tag.Equals("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag.Equals("Blue") && i == blueTerminator)
+        {
+            rend.enabled = false;
+        }
     }
 }
